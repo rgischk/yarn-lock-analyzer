2d9d714d0c3cca86135809247c1213401800d99e
Roman Gischkowski <rgischk@gmail.com>
2024-12-14 09:39:48 +0100

    Set up mui

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,21 @@ import "./index.css"
 import App from "./App.tsx"
 import { store } from "./store.ts"
 import { Provider } from "react-redux"
+import { CssBaseline, ThemeProvider } from "@mui/material"
+
+import "@fontsource/roboto/300.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
+import { theme } from "./theme.ts"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <CssBaseline />
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </StrictMode>,
 )
